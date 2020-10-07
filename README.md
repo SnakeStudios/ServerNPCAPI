9cd539d45e9ddc8eb81f0bdf559e2853745f55a6
iSnakeBuzz <contacto@isnakebuzz.com>
2020-10-07 16:01:18 -0300

    Changed README.md

--- README.md
+++ README.md
@@ -14,7 +14,28 @@ Dependency
 ```
 <dependency>
     <groupId>com.github.SnakeStudios</groupId>
-    <artifactId>ServerNPC-API</artifactId>
+    <artifactId>ServerNPCAPI</artifactId>
     <version>v1.0.1</version>
 </dependency>
+```
+
+Examples
+```java
+//Name of the NPC || 16 length longer.
+String name = "testNPC";
+
+// Location of the NPC
+Location loc = new Location(Bukkit.getWorld("world"), 0, 100, 0);
+
+// Settings for the NPC
+NPCSettings npcSettings = new NPCSettings();
+
+// UUID of the NPC
+UUID uuid = UUID.randomUUID();
+
+// Skin Information - Also you can use nicks.
+SkinData skinData = new SkinData("value", "signature", uuid);
+
+// Creating the NPC || It returns SnakeNPC if you need it.
+Main.getAPI().createNPC(name, uuid, npcSettings, skinData, loc, null);
 ```
--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,5 +1,4 @@
 import com.isnakebuzz.servernpc.Main;
-import com.isnakebuzz.servernpc.NPC.SnakeNPC;
 import com.isnakebuzz.servernpc.NPC.Utilities.NPCSettings;
 import com.isnakebuzz.servernpc.Skins.SkinData;
 import org.bukkit.Bukkit;
@@ -11,15 +10,23 @@ public class test {
 
     public void test() throws Exception {
 
-        Location loc = new Location(Bukkit.getWorld("world"), 0,  100, 0);
+        //Name of the NPC || 16 length longer.
+        String name = "testNPC";
+
+        // Location of the NPC
+        Location loc = new Location(Bukkit.getWorld("world"), 0, 100, 0);
+
+        // Settings for the NPC
         NPCSettings npcSettings = new NPCSettings();
 
+        // UUID of the NPC
         UUID uuid = UUID.randomUUID();
 
+        // Skin Information - Also you can use nicks.
         SkinData skinData = new SkinData("value", "signature", uuid);
 
-        // Automatically
-        SnakeNPC snakeNPC = Main.getAPI().createNPC("asdasd", uuid, npcSettings, skinData, loc, null);
+        // Creating the NPC || It returns SnakeNPC if you need it.
+        Main.getAPI().createNPC(name, uuid, npcSettings, skinData, loc, null);
 
     }
 
